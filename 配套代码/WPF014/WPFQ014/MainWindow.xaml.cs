f45116855e4dc4331d00be5455e57cd6298f4f3c
zmrbak <20995658@qq.com>
2020-02-21 22:55:12 +0800

    Add code

---
+++ 配套代码/WPF014/WPFQ014/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFQ014
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //获取当前窗口句柄
+            IntPtr handle = new WindowInteropHelper(this).Handle;
+
+            //启动notepad.exe
+            Process app = Process.Start("notepad.exe");
+
+            //主窗口句柄
+            IntPtr prsmwh = app.MainWindowHandle;
+
+            while (prsmwh == IntPtr.Zero)
+            {
+                prsmwh = app.MainWindowHandle;
+            }
+
+            //设置父窗口
+            SetParent(prsmwh, handle);
+
+            //子窗口最大化 
+            ShowWindowAsync(prsmwh, 3);
+
+        }
+
+        //声明调用user32.dll中的函数
+        [DllImport("user32.dll")]
+        static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+        [DllImport("user32.dll")]
+        static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);
+    }
+}
---
+++ 配套代码/WPF016/WPF016/HMIInputBox.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WPF016
+{
+    public class HMIInputBox:TextBox
+    {
+        public Boolean Alarm
+        {
+            get { return (Boolean)GetValue(AlarmProperty); }
+            set { SetValue(AlarmProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Alarm.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty AlarmProperty =
+            DependencyProperty.Register("Alarm", typeof(Boolean), typeof(HMIInputBox), new PropertyMetadata(OnValueChanged));
+
+    
+        private static void OnValueChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            HMIInputBox control = d as HMIInputBox;
+            if(control !=null)
+            {
+                control.UpdtateState();
+            }
+        }
+
+        private void UpdtateState()
+        {
+           if(Alarm == true)
+            {
+                VisualStateManager.GoToState(this, "AlarmState", true);
+            }
+            else
+            {
+                VisualStateManager.GoToState(this, "WarnState", true);
+            }
+        }
+    }
+}
