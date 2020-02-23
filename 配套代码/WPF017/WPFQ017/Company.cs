54105f43605a89e7c9e13c813bc8c9e3da56fbd4
zmrbak <20995658@qq.com>
2020-02-23 17:25:17 +0800

    add code

---
+++ 配套代码/WPF017/WPFQ017/Company.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFQ017
+{
+    //公司
+    public class Company
+    {
+        public String Name { get; set; }
+        public List<Department> Departments { set; get; }
+    }
+
+    //部门
+    public class Department
+    {
+        public String Name { get; set; }
+        public List<Employee> Employees { set; get; }
+    }
+
+    //员工
+    public class Employee
+    {
+        public String Name { get; set; }
+    }
+}
---
+++ 配套代码/WPF017/WPFQ017/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Serialization;
+
+namespace WPFQ017
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Employee employee1 = new Employee { Name = "员工1" };
+            Employee employee2 = new Employee { Name = "员工2" };
+            Employee employee3 = new Employee { Name = "员工3" };
+            Employee employee4 = new Employee { Name = "员工4" };
+            Employee employee5 = new Employee { Name = "员工5" };
+            Employee employee6 = new Employee { Name = "员工6" };
+            Employee employee7 = new Employee { Name = "员工7" };
+
+            Department department1 = new Department();
+            department1.Name = "部门1";
+            department1.Employees = new List<Employee>();
+            department1.Employees.Add(employee1);
+            department1.Employees.Add(employee2);
+            department1.Employees.Add(employee3);
+           
+
+            Department department2 = new Department();
+            department2.Name = "部门1";
+            department2.Employees = new List<Employee>();
+            department2.Employees.Add(employee4);
+            department2.Employees.Add(employee4);
+            department2.Employees.Add(employee6);
+            department2.Employees.Add(employee7);
+
+            Company company = new Company();
+            company.Name = "公司1";
+            company.Departments = new List<Department>();
+            company.Departments.Add(department1);
+
+            List<Company> companies = new List<Company>();
+            companies.Add(company);
+            companies.Add(company);
+            companies.Add(company);
+
+            tree1.ItemsSource = companies;
+
+            //XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<Company>));
+            //FileStream fs = new FileStream("Company.xml",FileMode.Create);
+            //xmlSerializer.Serialize(fs, companies);
+        }
+    }
+}
