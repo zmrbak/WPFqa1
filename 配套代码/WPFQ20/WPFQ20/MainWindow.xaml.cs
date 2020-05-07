6ed1fbcc60e922e30bffb27fbbd59b5d9f9702cd
zmrbak <20995658@qq.com>
2020-05-07 08:07:12 +0800

    add code

---
+++ 配套代码/WPFQ20/WPFQ20/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFQ20
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Grid grid = new Grid();
+            grid.Width = 50;
+            grid.Height = 50;
+            grid.Background = Brushes.AliceBlue;
+
+            //textBlock.Text = grid;
+            //textBlock.Inlines = grid;
+            //textBlock.con
+            //textBlock.c
+
+            textBlock.Inlines.Add(grid);
+            //InlineUIContainer inlineUI = new InlineUIContainer();
+            //inlineUI.Child = grid;
+
+            //textBlock.Inlines.Add(inlineUI);
+
+        }
+    }
+}
