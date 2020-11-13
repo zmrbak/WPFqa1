14e43905ddf5027a9ea4cf18d9d8ab5df6f1f093
zmrbak <20995658@qq.com>
2020-11-13 11:36:43 +0800

    add code

---
+++ 配套代码/WPFQ23/WpfApp1/CbConverter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WpfApp1
+{
+    public class CbConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null) return false;
+            if (parameter == null) return false;
+
+            //将value拆解成位的组合，判断第parameter位是否为1
+            if (int.TryParse(value.ToString(), out int selectCBox) && int.TryParse(parameter.ToString(), out int index))
+            {
+                if ((selectCBox & (1 << index)) != 0)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null) return false;
+            if (parameter == null) return false;
+
+            //解析参数
+            if (bool.TryParse(value.ToString(), out bool selectCBox) && int.TryParse(parameter.ToString(), out int index))
+            {
+                if (selectCBox)
+                {
+                    SelectCBox = SelectCBox | (1 << index);
+                }
+                else
+                {
+                    SelectCBox = SelectCBox & ~(1 << index);
+                }
+            }
+            return SelectCBox;
+        }
+
+        //添加一个额外的参数保存选中结果
+        public static int SelectCBox;
+    }
+}
---
+++ 配套代码/WPFQ23/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            SelectCBox = (1 << 1) | (1 << 2);
+
+        }
+
+
+        //整数32位
+        //0101-----32位
+
+        public int SelectCBox
+        {
+            get { return (int)GetValue(SelectCBoxProperty); }
+            set
+            {
+                SetValue(SelectCBoxProperty, value);
+                CbConverter.SelectCBox = value;
+            }
+        }
+
+        // Using a DependencyProperty as the backing store for SelectCBox.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty SelectCBoxProperty =
+            DependencyProperty.Register("SelectCBox", typeof(int), typeof(MainWindow), new PropertyMetadata(default(int)));
+    }
+}
