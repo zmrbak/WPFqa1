fb9efe9a8ea8737d25e11d6f880082b43fa1150f
zmrbak <20995658@qq.com>
2021-10-13 07:52:29 +0800

    add code

---
+++ 配套代码/WPFQ27/WpfQ27/BindablePasswordBox.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfQ27
+{
+    /// <summary>
+    /// BindablePasswordBox.xaml 的交互逻辑
+    /// </summary>
+    public partial class BindablePasswordBox : UserControl
+    {
+        public BindablePasswordBox()
+        {
+            InitializeComponent();
+        }
+        private bool isPasswordChanging;
+
+
+        public string Password
+        {
+            get { return (string)GetValue(PasswordProperty); }
+            set { SetValue(PasswordProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Password.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty PasswordProperty =
+            DependencyProperty.Register("Password", typeof(string), typeof(BindablePasswordBox),
+                new FrameworkPropertyMetadata(default(string),FrameworkPropertyMetadataOptions.BindsTwoWayByDefault ,OnPasswordChanged,null,false,UpdateSourceTrigger.PropertyChanged));
+
+        private static void OnPasswordChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if(d is BindablePasswordBox bindablePassword)
+            {
+                bindablePassword.UpdatePassword();
+            }           
+        }
+
+        private void UpdatePassword()
+        {
+            if(isPasswordChanging==false)
+            {
+                passwordBox.Password = Password;
+            }
+        }
+
+        private void passwordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            isPasswordChanging=true;
+            Password = this.passwordBox.Password;
+            isPasswordChanging = false;
+        }
+    }
+}
---
+++ 配套代码/WPFQ27/WpfQ27/MainViewModel.cs
@@ -0,0 +1,48 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfQ27
+{
+    public class MainViewModel : BindableBase
+    {
+        private string userName="user";
+
+        public string UserName
+        {
+            get { return userName; }
+            set
+            {
+                SetProperty(ref userName, value);
+            }
+        }
+
+        private string password="222";
+
+        public string Password
+        {
+            get { return password; }
+            set
+            {
+                SetProperty(ref password, value);
+            }
+        }
+
+        public MainViewModel()
+        {
+            LoginCommand = new DelegateCommand(OnLogin);
+        }
+
+        public DelegateCommand LoginCommand { get; }
+
+        private void OnLogin()
+        {
+            MessageBox.Show("UserName：" + userName + "\nPassword:" + password);
+        }
+    }
+}
