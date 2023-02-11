ab6ee7df46cca3a2cc5ded05b570198e19672f8d
Zmrbak <20995658@qq.com>
2023-02-11 17:27:31 +0800

    add code

---
+++ 配套代码/WPFQ36/wpfq36/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wpfq36
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.DataContext=new MainWindowViewModel();
+        }
+
+        private void CheckBox_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is CheckBox checkBox)
+            {
+                if (checkBox.IsChecked == true)
+                {
+                    this.tb1.Text = "111";
+                }
+                else
+                {
+                    this.tb1.Text = "222";
+                }
+            }
+        }
+    }
+}
---
+++ 配套代码/WPFQ36/wpfq36/MainWindowViewModel.cs
@@ -0,0 +1,37 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpfq36
+{
+    public partial class MainWindowViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string textBlockA = "222";
+
+        [ObservableProperty]
+        private string textBlockB = "111";
+
+        [ObservableProperty]
+        private string textBlockC = "222";
+
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(TextBlockC))]
+        private bool isCheckBoxChecked;
+
+        partial void OnIsCheckBoxCheckedChanged(bool value)
+        {
+            TextBlockC = value ? TextBlockB : TextBlockA;
+        }
+
+        [RelayCommand]
+        private void CBChanged(bool arg)
+        {
+            TextBlockC = arg ? TextBlockB : TextBlockA;
+        }
+    }
+}
